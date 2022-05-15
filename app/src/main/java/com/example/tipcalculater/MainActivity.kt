83e118fb742db0bb513b3fde8ad9a258e3753fb6
Jagi <ba87688@gmail.com>
2022-05-15 03:34:13 -0500

    Added number of people to divide total amount by.

--- app/src/main/java/com/example/tipcalculater/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculater/MainActivity.kt
@@ -5,21 +5,27 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.widget.EditText
-import android.widget.SeekBar
-import android.widget.TextView
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.tipcalculater.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputLayout
 
 private const val TAG = "MainActivity"
-private const val INITIAL_TIP =15
+private const val INITIAL_TIP = 15
+
 class MainActivity : AppCompatActivity() {
     //viewBiding
     private lateinit var binding: ActivityMainBinding
     private lateinit var etBaseAmount: EditText
-    private lateinit var seekBarTip : SeekBar
-    private lateinit var tvTipPercentLabel:TextView
-    private lateinit var tvTipAmount : TextView
-    private lateinit var tvTotalAmount :TextView
+    private lateinit var seekBarTip: SeekBar
+    private lateinit var tvTipPercentLabel: TextView
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
+    private lateinit var totalPerPerson: TextView
+    private lateinit var perPersonLabel: TextView
+    private lateinit var numberOfPeople: AutoCompleteTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +33,11 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+
         //edittext for bill amount
         etBaseAmount = binding.etBaseAmount
         //tip seeker bar
         seekBarTip = binding.tipSeekBar
-
 
         //actual tip
         tvTipAmount = binding.textViewTipAmount
@@ -40,23 +46,32 @@ class MainActivity : AppCompatActivity() {
         tvTotalAmount = binding.textViewTotal
 
         tvTipPercentLabel = binding.percentageTip
+        numberOfPeople = binding.autoCompleteTextView
 
+
+        totalPerPerson = binding.totalPerPerson
+        perPersonLabel = binding.perPersonLabel
 
         //set initial value
         seekBarTip.progress = INITIAL_TIP
         tvTipPercentLabel.text = "$INITIAL_TIP%"
+        totalPerPerson.text = ""
+//        totalPerPerson.text = "${numberOfPeople.text.toString().toInt()}"
 
-//        etBaseAmount.setText("0")
-        Log.i(TAG, "HELLLOOOOO: $etBaseAmount")
 
+        //Hide keyboard when clickout of edittext
+        binding.root.setOnClickListener {
+            (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager).apply {
+                hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+            }
+            etBaseAmount.clearFocus()
+        }
 
         //seekbar listener
-        seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 Log.i(TAG, "onProgressChanged: $progress")
                 tvTipPercentLabel.text = "$progress%"
-//                tvTipAmount.text = "$progress%"
-//                tvTotalAmount.text = "$progress%"
                 calcTipAndTotal()
 
             }
@@ -71,7 +86,7 @@ class MainActivity : AppCompatActivity() {
         })
 
         //edittext listener
-        etBaseAmount.addTextChangedListener(object: TextWatcher{
+        etBaseAmount.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
@@ -87,15 +102,30 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+        numberOfPeople.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.i(TAG, "afterTextChanged: INSIDE 3d stuff ${s}")
+                calcTipAndTotal()
+            }
+
+        })
+
+
     }
 
     private fun calcTipAndTotal() {
-        if(etBaseAmount.text.isEmpty()){
+        if (etBaseAmount.text.isEmpty()) {
             tvTipAmount.text = ""
             tvTotalAmount.text = ""
             return
 
-        }else {
+        } else {
             //this method is used to 1. get value of base total and tip percentage. 2. calculate the tip
             //3. find total and update the user interface
             val baseAmount = etBaseAmount.text.toString().toDouble()
@@ -103,12 +133,27 @@ class MainActivity : AppCompatActivity() {
             val tipPercentage = seekBarTip.progress
             //calc tip total
             val tipAmount = baseAmount * tipPercentage / 100
-            val totalAmount = baseAmount + tipAmount
+            var totalAmount = baseAmount + tipAmount
+
+            var numberOfPpl = numberOfPeople.text.toString().toInt()
+
 
 //        update ui
             tvTipAmount.text = "%.2f".format(tipAmount)
             tvTotalAmount.text = "%.2f".format(totalAmount)
+
+            val perPersonAmount = "%.2f".format(totalAmount / numberOfPpl)
+            totalPerPerson.text = perPersonAmount.toString()
+
         }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        val languages = resources.getStringArray(R.array.number_of_people)
+        val arrayAdapter = ArrayAdapter(this, R.layout.drop_down_item, languages)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
     }
 
 }
